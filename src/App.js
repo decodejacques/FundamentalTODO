f8f74e95a1a6b3205797d3696b6997fd31f2bccb
rathereasy@gmail.com <jacques@decodemtl>
2017-12-19 10:39:34 -0500

    Question 2

--- src/App.js
+++ src/App.js
@@ -3,10 +3,20 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = { number: 0 }
+  }
+  componentDidMount() {
+    setInterval(
+      () => this.setState({ number: Math.random() }),
+      1000);
+  }
+
   render() {
     return (
       <div className="App">
-       42
+        {this.state.number}
       </div>
     );
   }
